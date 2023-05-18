f60f941f7eabe02fe083d8992a3c2a1a531011c7
arsalan_qadir <arsalan.qadir@otwosoft.com>
2023-05-18 21:38:14 +0500

    add form in login page

--- src/app/login.tsx
+++ src/app/login.tsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 function Login() {
   return (
-    <div>login</div>
+    <div className='h-screen w-full bg-custombgcolor flex justify-center items-center '>
+      <div className='bg-blend-hard-light p-10 flex justify-center items-center flex-col'>
+      <p className=" uppercase text-4xl font-sans text-linkcolor">login</p>
+      <form>
+        <input type='text' placeholder='Enter Email...' className='w-full h-12 mt-4 border-2 border-gray-500 rounded-md'/>
+        <input type='password' placeholder='Enter Password...' className='w-full h-12 mt-4 border-2 border-gray-500 rounded-md'/>
+        <button className=' h-12 mt-4'>Login</button>
+      </form>
+      </div>
+    </div>
   )
 }
 
