62bb3c0dc9fb3ebcd78c15a6c77da5569137f467
Arfan Liaqat <arfanliaqat10@gmail.com>
2023-05-21 00:46:18 +0500

    feat: add next-auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,7 @@
-import Image from 'next/image'
-import Dashboard from './dashboard'
-import MainBanner from '@/components/MainBanner'
-import Login from './login'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
-      {/* @ts-ignore */}
-      {/* <MainBanner/>
-      <Dashboard/> */}
-      <Login/>
+      <h1>Home page which is protected</h1>
     </main>
-  )
+  );
 }
